01b7599ed9ea0a513d8eac3a482747a0d2aa5345
ngocdg103 <ngocdg1003@gmail.com>
2026-04-05 01:17:24 +0700

    HomeAssignment

--- Lab02/AimsProject/src/Aims.java
+++ Lab02/AimsProject/src/Aims.java
@@ -1,5 +1,30 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+public class Aims {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc(
+            "Cars", "Animation", "John Lasseter", 117, 19.95f
+        );
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc(
+            "The Departed", "Crime/Drama", "Martin Scorsese", 151, 24.95f
+        );
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc(
+            "Pirates of the Caribbean: Dead Man's Chest", "Action/Adventure", "Gore Verbinski", 151, 29.95f
+        );
+
+        anOrder.addDigitalVideoDisc(dvd1);
+        anOrder.addDigitalVideoDisc(dvd2);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        System.out.println("\n--- Order Status ---");
+        anOrder.printCart();
+
+        System.out.println("\nTesting removal:");
+        anOrder.removeDigitalVideoDisc(dvd1);
+
+        System.out.println("Total cost is: " + anOrder.totalCost() + "$");
     }
 }
